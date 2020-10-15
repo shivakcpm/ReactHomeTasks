c25f0bad10550939c1106908238f68ebdf970bf2
Shivakrishna.Kongara <shivakrishna.kongara@thomsonreuters.com>
2020-10-15 16:39:11 +0530

    homework -8 tasks

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './pageNotFound.css';
 import { useHistory } from 'react-router-dom';
 
-const PageNotFound =  () => {
-    const history = useHistory();
+const PageNotFound = () => {
+  const history = useHistory();
   return (
     <div className="container">
       <div className="header-bar">
@@ -11,11 +11,16 @@ const PageNotFound =  () => {
         <span>roulette</span>
       </div>
       <div className="page-body">
-      <div class="page-info">Page Not Found</div>
-      <img src="/public/404.PNG"></img>
-      <button className="go-back" onClick={() => {
-history.push('/');
-}}>GO BACK TO HOME</button>
+        <div class="page-info">Page Not Found</div>
+        <img src="/public/404.PNG"></img>
+        <button
+          className="go-back"
+          onClick={() => {
+            history.push('/');
+          }}
+        >
+          GO BACK TO HOME
+        </button>
       </div>
     </div>
   );
