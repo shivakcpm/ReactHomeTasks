ea80d29eba1a339d0a9bc584aabc417d581b247a
Shiva Krishna Kongara <ShivaKrishna.Kongara@gmail.com>
2020-11-04 19:00:29 +0530

    Resolved review comments

--- src/store/rootReducer.test.js
+++ src/store/rootReducer.test.js
@@ -1,28 +1,27 @@
-import  rootReducer,{initialState} from './rootReducer';
+import  rootReducer, {initialState} from './rootReducer';
 import * as actions from './actions';
-
 
 describe('rootReducer', () => {
     it('should return the initial state', () => {
-      expect(rootReducer(undefined,{type:''})).toEqual(initialState);
+      expect(rootReducer(undefined, {type:''})).toEqual(initialState);
     });
 
     it('should return the get movie state', () => {
-        let payload = {moviesData:{data:[]}};
-        expect(rootReducer(undefined,actions.getMoviesAction(payload))).toEqual(payload);
+        const payload = {moviesData:{data:[]}};
+        expect(rootReducer(undefined, actions.getMoviesAction(payload))).toEqual(payload);
     });
 
     it('should return the edit movie state', () => {
-        let payload = {moviesData:{data:[]}};
-        expect(rootReducer(undefined,actions.editMovieAction(payload))).toEqual(payload);
+        const payload = {moviesData:{data:[]}};
+        expect(rootReducer(undefined, actions.editMovieAction(payload))).toEqual(payload);
     });
 
     it('should return the add movie state', () => {
-        let payload = {moviesData:{data:[]}};
-        expect(rootReducer(undefined,actions.addMovieAction(payload))).toEqual(payload);
+        const payload = {moviesData:{data:[]}};
+        expect(rootReducer(undefined, actions.addMovieAction(payload))).toEqual(payload);
     });
     it('should return the delete movie state', () => {
-        let payload = {moviesData:{data:[]}};
-        expect(rootReducer(undefined,actions.deleteMovieAction(payload))).toEqual(payload);
+        const payload = {moviesData:{data:[]}};
+        expect(rootReducer(undefined, actions.deleteMovieAction(payload))).toEqual(payload);
     });
 });
