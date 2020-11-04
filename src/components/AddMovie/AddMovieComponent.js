77a6213a7dafbd286fab5d45bd4a749b2fc8e460
Shiva Krishna Kongara <ShivaKrishna.Kongara@gmail.com>
2020-11-04 16:45:14 +0530

    server side rendering

--- src/components/AddMovie/AddMovieComponent.js
+++ src/components/AddMovie/AddMovieComponent.js
@@ -6,7 +6,6 @@ import { store, addMovieAsync, editMovieAsync } from '../../store/store';
 import './AddMovie.css';
 
 export default class AddMovieComponent extends Component {
-
   isDataValid(values) {
     const { title, release_date, poster_path, genres, overview, runtime } = values;
     const errors = {};
